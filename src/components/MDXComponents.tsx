0e9e5d22e5649e055f52fc6c3c8eb5766635bba6
cocozzang <cocozz503@naver.com>
2024-11-26 16:49:49 +0900

    fix: 인라인코드와 코드블럭 구분시켜 렌더링

--- src/components/MDXComponents.tsx
+++ src/components/MDXComponents.tsx
@@ -3,12 +3,28 @@ import CodeBlock from "./CodeBlock";
 
 const components: MDXComponents = {
   pre: ({ children, ...props }) => (
-    <pre {...props} className="relative">
+    <pre {...props} className="relative group">
       {children}
     </pre>
   ),
   code: (props) => {
-    return <CodeBlock {...props} />;
+    const isCodeBlock =
+      // pre 태그 내부의 code인지 확인
+      props.className?.includes("language-") ||
+      // 여러 줄 코드인지 확인
+      props.children?.toString().includes("\n");
+
+    if (isCodeBlock) {
+      return <CodeBlock {...props} />;
+    }
+
+    // 인라인 코드
+    return (
+      <code
+        {...props}
+        className="px-1.5 py-0.5 rounded-md bg-slate-100 dark:bg-slate-800 text-slate-900 dark:text-slate-100 text-[0.9em] font-normal"
+      />
+    );
   },
 };
 
